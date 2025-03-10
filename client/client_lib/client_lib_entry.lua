583dccb0299403f1e100403823d0e340a54e6350
CarlBishop <peppino@galad.it>
2025-03-10 14:50:37 +0100

    Update client_lib_entry.lua to ox

--- client/client_lib/client_lib_entry.lua
+++ client/client_lib/client_lib_entry.lua
@@ -5,10 +5,11 @@ function isOwner(entity)
      if not oilrig then return print('failed to get oilwell') end
      local is_employee = nil
      local is_owner = nil
-     -- await didn't work!
+     
      QBCore.Functions.TriggerCallback('keep-oilwell:server:is_employee', function(_is_employee, _is_owner)
           is_employee, is_owner = _is_employee, _is_owner
      end, oilrig.oilrig_hash)
+     
      for i = 1, 5, 1 do
           if is_employee ~= nil then
                break
@@ -18,6 +19,7 @@ function isOwner(entity)
      return is_employee, is_owner
 end
 
+-- Helper functions remain the same
 local function Draw2DText(content, font, colour, scale, x, y)
      SetTextFont(font)
      SetTextScale(scale, scale)
@@ -38,10 +40,8 @@ local function RotationToDirection(rotation)
           z = (math.pi / 180) * rotation.z
      }
      local direction = {
-          x = -math.sin(adjustedRotation.z) *
-              math.abs(math.cos(adjustedRotation.x)),
-          y = math.cos(adjustedRotation.z) *
-              math.abs(math.cos(adjustedRotation.x)),
+          x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)),
+          y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)),
           z = math.sin(adjustedRotation.x)
      }
      return direction
@@ -57,14 +57,13 @@ local function RayCastGamePlayCamera(distance)
           z = cameraCoord.z + direction.z * distance
      }
      local a, b, c, d, e = GetShapeTestResult(
-          StartShapeTestRay(cameraCoord.x, cameraCoord.y,
-               cameraCoord.z, destination.x,
-               destination.y, destination.z,
-               -1, PlayerPedId(), 0))
+          StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, 
+          destination.x, destination.y, destination.z, -1, PlayerPedId(), 0))
      return c, e
 end
 
 function ChooseSpawnLocation()
+     -- This function remains largely the same
      local plyped = PlayerPedId()
      local pedCoord = GetEntityCoords(plyped)
      local activeLaser = true
@@ -73,41 +72,27 @@ function ChooseSpawnLocation()
 
      while activeLaser do
           Wait(0)
-          local color = {
-               r = 2,
-               g = 241,
-               b = 181,
-               a = 200
-          }
+          local color = { r = 2, g = 241, b = 181, a = 200 }
           local position = GetEntityCoords(plyped)
           local coords, entity = RayCastGamePlayCamera(1000.0)
-          Draw2DText('Press ~g~E~w~ To Place oilwell', 4, { 255, 255, 255 }, 0.4, 0.43,
-               0.888 + 0.025)
+          Draw2DText('Press ~g~E~w~ To Place oilwell', 4, { 255, 255, 255 }, 0.4, 0.43, 0.888 + 0.025)
           if IsControlJustReleased(0, 38) then
                activeLaser = false
                DeleteEntity(oilrig)
                return coords
           end
-          DrawLine(position.x, position.y, position.z, coords.x, coords.y,
-               coords.z, color.r, color.g, color.b, color.a)
+          DrawLine(position.x, position.y, position.z, coords.x, coords.y, coords.z, color.r, color.g, color.b, color.a)
           SetEntityCollision(oilrig, false, false)
           SetEntityCoords(oilrig, coords.x, coords.y, coords.z, 0.0, 0.0, 0.0, 0)
      end
 end
 
+-- Blip functions remain the same
 function createCustom(coord, o)
-     local blip = AddBlipForCoord(
-          coord.x,
-          coord.y,
-          coord.z
-     )
+     local blip = AddBlipForCoord(coord.x, coord.y, coord.z)
      SetBlipSprite(blip, o.sprite)
      SetBlipColour(blip, o.colour)
-     if o.range == 'short' then
-          SetBlipAsShortRange(blip, true)
-     else
-          SetBlipAsShortRange(blip, false)
-     end
+     SetBlipAsShortRange(blip, o.range == 'short')
      BeginTextCommandSetBlipName("STRING")
      AddTextComponentString(replaceString(o))
      EndTextCommandSetBlipName(blip)
@@ -117,70 +102,70 @@ end
 function replaceString(o)
      local s = o.name
      if o.id ~= nil then
-          --  oilwells
           local oilrig = OilRigs:getById(o.id)
           s = s:gsub("OILWELLNAME", oilrig.name)
           s = s:gsub("OILWELL_HASH", oilrig.oilrig_hash)
           s = s:gsub("DB_ID_RAW", o.id)
           s = s:gsub("TYPE", o.type)
-
      else
           s = s:gsub("TYPE", o.type)
      end
      return s
 end
 
+-- Convert qb-target to ox_target
 function createOwnerQbTarget(hash, coord)
-     exports['qb-target']:RemoveZone("oil-rig-" .. hash)
-     Targets.qb_target.oilwell(coord, hash)
+     exports.ox_target:removeZone("oil-rig-" .. hash)
+     Targets.ox_target.oilwell(coord, hash)
 end
 
 RegisterNetEvent('keep-oilwell:client:remove_oilwell', function(data)
      local oilwell = OilRigs:getByEntityHandle(data.entity)
+     
      for i = 1, 3, 1 do
           local value = RandomHash(4)
-          local inputData = exports['qb-input']:ShowInput({
-               header = 'Enter This Values (' .. value .. ')',
-               inputs = {
-                    {
-                         type = 'text',
-                         isRequired = true,
-                         name = 'RandomHash',
-                         text = ''
-                    },
+          local input = lib.inputDialog('Enter This Values (' .. value .. ')', {
+               {
+                    type = 'input',
+                    label = 'Verification Code',
+                    description = 'Enter the code shown above',
+                    required = true
                }
           })
-          if not inputData then
+
+          if not input then
                QBCore.Functions.Notify('Canceled', "primary")
                return
           end
-          if inputData.RandomHash ~= value then
+          
+          if input[1] ~= value then
                QBCore.Functions.Notify('Failed', "primary")
                return
           end
      end
+     
      TriggerServerEvent('keep-oilwell:server:remove_oilwell', oilwell.oilrig_hash)
 end)
 
+-- Convert Add_3rd_eye to use ox_target
 function Add_3rd_eye(coord, Type)
      local key = Type
      if key == 'storage' then
-          Targets.qb_target.storage(coord, key)
+          Targets.ox_target.storage(coord, key)
      elseif key == 'distillation' then
-          Targets.qb_target.distillation(coord, key)
+          Targets.ox_target.distillation(coord, key)
      elseif key == 'blender' then
-          Targets.qb_target.blender(coord, key)
+          Targets.ox_target.blender(coord, key)
      elseif key == 'barrel_withdraw' then
-          Targets.qb_target.barrel_withdraw(coord, key)
+          Targets.ox_target.barrel_withdraw(coord, key)
      elseif key == 'crude_oil_transport' then
-          Targets.qb_target.crude_oil_transport(coord, key)
+          Targets.ox_target.crude_oil_transport(coord, key)
      elseif key == 'toggle_job' then
-          Targets.qb_target.toggle_job(coord, key)
+          Targets.ox_target.toggle_job(coord, key)
      end
 end
 
----force remove objects in area
----@param coord table
+-- These events remain the same
 RegisterNetEvent('keep-oilrig:client:clearArea', function(coord)
      ClearAreaOfObjects(coord.x, coord.y, coord.z, 5.0, 1)
 end)
